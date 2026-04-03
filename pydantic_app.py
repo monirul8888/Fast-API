0a3bf0752a3316d64b6bb7617ebd8937fc738708
monirul8888 <monirul429588@gmail.com>
2026-04-03 22:37:55 +0600

    Added Optional

--- pydantic_app.py
+++ pydantic_app.py
@@ -1,23 +1,23 @@
 from pydantic import BaseModel
-from typing import List, Dict
+from typing import List, Dict, Optional
 class Patients (BaseModel):
     name: str
     age: int
-    married: bool
-    alergics: List [str]
+    married: Optional[bool]= False
+    alergics: Optional[List [str]] = None
     contact : Dict [str, str]
 
 
 def insert(patient: Patients):
     print(patient.name)
     print(patient.age)
+    print(patient.alergics)
     print(patient.contact)
     print("Inserted Successfully")
 
 patient_info = {"name": "Monirul",
                 "age": 25,
                 "married": False,
-                "alergics": ["Begun"],
                 "contact":{
                     "Phone": "01742958888",
                     "Email": "monirul.cr3@gmail.com"
